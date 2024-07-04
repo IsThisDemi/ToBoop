b4815711de183747486de950bd9d3586bcf35cd6
IsThisDemi <furryalt68@gmail.com>
2024-07-04 12:09:38 +0200

    Initial commit

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using Avalonia.Controls;
+using ToBoop.Services;
+using ToBoop.ViewModels;
+
+namespace ToBoop.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
